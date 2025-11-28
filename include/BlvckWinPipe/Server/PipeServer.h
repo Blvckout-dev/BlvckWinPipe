30f19e7d1a762c2339e692ac23c0b0a183c8e0b9
Maximilian <Maximilian0712@outlook.de>
2025-11-28 09:54:07 +0100

    feat(server): Add PipeServer class and initial implementation

---
+++ include/BlvckWinPipe/Server/PipeServer.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include "BlvckWinPipe/Export.h"
+
+namespace Blvckout::BlvckWinPipe::Server
+{
+    class BLVCKWINPIPE_API PipeServer
+    {
+    private:
+        
+    public:
+        PipeServer();
+        ~PipeServer();
+    };
+} // namespace Blvckout::BlvckWinPipe::Server
---
+++ src/Server/PipeServer.cpp
@@ -0,0 +1,12 @@
+#include "BlvckWinPipe/Server/PipeServer.h"
+
+namespace Blvckout::BlvckWinPipe::Server
+{
+    PipeServer::PipeServer()
+    {
+    }
+
+    PipeServer::~PipeServer()
+    {
+    }
+} // namespace Blvckout::BlvckWinPipe::Server
