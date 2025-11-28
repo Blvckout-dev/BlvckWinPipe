8fddf38f02fd3eb32245d4f17293f75996c24bfb
Maximilian <Maximilian0712@outlook.de>
2025-11-28 10:04:59 +0100

    feat(examples): Add initial EchoServer example

---
+++ examples/EchoServer/EchoServer.cpp
@@ -0,0 +1,10 @@
+#include <BlvckWinPipe/Server/PipeServer.h>
+
+using namespace Blvckout::BlvckWinPipe::Server;
+
+int main(int argc, char const *argv[])
+{
+    PipeServer server;
+
+    return 0;
+}
