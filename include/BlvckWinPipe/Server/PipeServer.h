cd991f4f3e77e9e2c56ba3f51f258973cec7b795
Maximilian <Maximilian0712@outlook.de>
2025-12-01 00:45:24 +0100

    feat(PipeServer): Add IOCP handle, worker thread pool, and running state
    - _MaxWorkerThreads is at least 2 by default
    - Initialize _IOCP to INVALID_HANDLE_VALUE

--- include/BlvckWinPipe/Server/PipeServer.h
+++ include/BlvckWinPipe/Server/PipeServer.h
@@ -1,17 +1,32 @@
 #pragma once
 
+#include <atomic>
 #include <string>
+#include <thread>
+#include <vector>
 
 #include "BlvckWinPipe/Export.h"
+#include "BlvckWinPipe/Utils/WinHandle.h"
 
 namespace Blvckout::BlvckWinPipe::Server
 {
+    using Utils::Windows::WinHandle;
+
     class BLVCKWINPIPE_API PipeServer
     {
     private:
         inline static constexpr wchar_t PIPE_NAME_PREFIX[] = L"\\\\.\\pipe\\";
         std::wstring _Name;
         std::wstring _PipeName;
+
+        WinHandle _IOCP;
+
+        size_t _MaxWorkerThreads;
+        std::vector<std::thread> _Workers;
+
+        std::atomic<bool> _IsRunning { false };
+
+        void WorkerThread();
     public:
         PipeServer(const std::wstring& name);
         ~PipeServer();
@@ -23,5 +38,8 @@ namespace Blvckout::BlvckWinPipe::Server
         }
 
         std::wstring PipeName() const { return _PipeName; }
+
+        void Start();
+        void Stop() noexcept;
     };
 } // namespace Blvckout::BlvckWinPipe::Server
--- src/Server/PipeServer.cpp
+++ src/Server/PipeServer.cpp
@@ -1,10 +1,16 @@
 #include "BlvckWinPipe/Server/PipeServer.h"
 
+#include "BlvckWinPipe/Platform/Platform.h"
+
 namespace Blvckout::BlvckWinPipe::Server
 {
     PipeServer::PipeServer(const std::wstring& name) :
         _Name(name),
-        _PipeName(std::wstring(PIPE_NAME_PREFIX) + name)
+        _PipeName(std::wstring(PIPE_NAME_PREFIX) + name),
+        _IOCP(INVALID_HANDLE_VALUE),
+        _MaxWorkerThreads(
+            std::max(std::thread::hardware_concurrency(), 2u)
+        )
     {
     }
 
